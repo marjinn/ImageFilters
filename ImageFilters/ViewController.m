9c63835e4137243365ab1a6a12d18505d50c1e44
mar Jinn <marjinn@mars-Mac.local>
2014-10-13 11:10:58 +0530

    c2- save asset with phasset

--- ImageFilters/ViewController.m
+++ ImageFilters/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 #import "CITipsAndTryOuts.h"
 
+#import <ImageIO/ImageIO.h>
+
+@import AssetsLibrary;
+@import Photos;
+
 @interface ViewController ()
 
 @end
@@ -18,24 +23,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ///cpu context - when saving image to photo roll
     
-    /*Displaying an image in UIIMageView */
-    UIImageView* imgView = nil;
-    imgView =
-    [[UIImageView alloc] initWithFrame:[[self view] bounds]];
+    //RENDERING CIImage  using CPU
     
-    [[self view] addSubview:imgView];
-    
-    //input Image
-    UIImage* image = nil;
-    image = [UIImage imageNamed:@"arrow.jpg"];
-    
-    CGImageRef cg_image = NULL;
-    cg_image = [image CGImage];
+    static NSString* const imageName = @"arrow.jpg";
     
     //create CIImage
+    NSURL* imageURL = nil;
+    imageURL =
+    [[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:imageName];
+    
+    //using CIImage imageWIthData or imageWithContentsOFURL
+    //gives lot of metatdata informataion
+    
     CIImage* ci_image = nil;
-    ci_image = [CIImage imageWithCGImage:cg_image];
+    ci_image = [CIImage imageWithContentsOfURL:imageURL];
     
     //create a CIFilter and set input values
     CIFilter* ci_sepiaFilter = nil;
@@ -44,13 +47,174 @@
     [ci_sepiaFilter setValue:ci_image forKey:kCIInputImageKey];
     [ci_sepiaFilter setValue:[NSNumber numberWithFloat:0.8f]
                       forKey:@"inputIntensity"];
-    
-
-    //create context
-    CIContext* context = nil;
 
     
+    //create CPU Context
+    NSDictionary* options = nil;
+    options = @{ kCIContextUseSoftwareRenderer : @YES };
     
+    CIContext* cpuContext = nil;
+    cpuContext =
+    [CIContext contextWithOptions:options];
+    
+    
+    //create CGImage from CIImage
+    CIImage* outputImage = nil;
+    outputImage = [ci_sepiaFilter outputImage];
+    
+    CGImageRef cgiimg = nil;
+    cgiimg =
+    [cpuContext createCGImage:outputImage fromRect:[outputImage extent]];
+    
+//    //Add CGIImage to photo library
+//    ALAssetsLibrary* library = nil;
+//    library = [ALAssetsLibrary new];
+//    
+//    [library writeImageToSavedPhotosAlbum:cgiimg
+//                                 metadata:[outputImage properties]
+//                          completionBlock:
+//     ^(NSURL *assetURL, NSError *error)
+//    {
+//        CGImageRelease(cgiimg);
+//    }
+//     ];
+    
+    __block PHAssetChangeRequest* createAssetRequest = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:
+     ^{
+         
+         createAssetRequest =
+         [PHAssetChangeRequest creationRequestForAssetFromImage:[UIImage imageWithCGImage:cgiimg]];
+     }
+                                      completionHandler:
+     ^(BOOL success, NSError *error)
+    {
+        
+        
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            PHAssetCollection* assetCollection = nil;
+            assetCollection = (PHAssetCollection*)
+            [[PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil] lastObject];
+            
+            PHAssetCollectionChangeRequest* assetCollectionChangeRequest = nil;
+            assetCollectionChangeRequest =
+            [PHAssetCollectionChangeRequest changeRequestForAssetCollection:(PHAssetCollection *)assetCollection];
+            
+            [assetCollectionChangeRequest addAssets:
+             (id<NSFastEnumeration>)@[[createAssetRequest placeholderForCreatedAsset]]];
+        }
+                                          completionHandler:
+         ^(BOOL success, NSError *error) {
+            
+        }];
+        
+        
+        
+        
+    }
+     ];
+    
+   
+}
+-(void) applyAnImageToImageView
+{
+/*Displaying an image in UIIMageView */
+UIImageView* imgView = nil;
+imgView =
+[[UIImageView alloc] initWithFrame:[[self view] bounds]];
+
+[[self view] insertSubview:imgView atIndex:0];
+imgView = nil;
+
+NSString* resourcePath = nil;
+resourcePath =
+[[NSBundle mainBundle] resourcePath];
+
+static NSString* const imageName = @"arrow.jpg";
+
+NSString* imagePath = nil;
+imagePath = [resourcePath stringByAppendingPathComponent:imageName];
+
+//    NSData* imageData = nil;
+//    imageData =
+//    [NSData dataWithContentsOfFile:imagePath];
+
+//input Image
+//    UIImage* image = nil;
+//    image = [UIImage imageWithData:imageData];
+
+//    CGImageRef cg_image = NULL;
+//    cg_image = [image CGImage];
+
+//create CIImage
+NSURL* imageURL = nil;
+imageURL =
+[[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:imageName];
+
+//using CIImage imageWIthData or imageWithContentsOFURL
+//gives lot of metatdata informataion
+
+CIImage* ci_image = nil;
+ci_image = [CIImage imageWithContentsOfURL:imageURL];
+
+//create a CIFilter and set input values
+CIFilter* ci_sepiaFilter = nil;
+ci_sepiaFilter = [CIFilter filterWithName:@"CISepiaTone"];
+
+[ci_sepiaFilter setValue:ci_image forKey:kCIInputImageKey];
+[ci_sepiaFilter setValue:[NSNumber numberWithFloat:0.8f]
+                  forKey:@"inputIntensity"];
+
+//create context
+CIContext* context = nil;
+context = [CIContext contextWithOptions:nil];
+
+//get output from filter
+CIImage* outputCIimage = nil;
+outputCIimage = [ci_sepiaFilter outputImage];//
+
+//render CIImage into CGImageRef
+CGImageRef cgiimg = NULL;
+cgiimg =
+[context createCGImage:outputCIimage fromRect:[outputCIimage extent]];
+
+//get image orientation
+//if no value found the 1 ie UIImageORienttaionUp is assumed
+int imageOrientation =
+(int)[outputCIimage properties][(__bridge NSString*) kCGImagePropertyTIFFDictionary][(__bridge NSString*)kCGImagePropertyTIFFOrientation];
+
+
+NSDictionary* tiffDictionary = nil;
+tiffDictionary =
+[[outputCIimage properties]
+ objectForKey:(__bridge NSString*) kCGImagePropertyTIFFDictionary];
+
+
+
+
+UIImage* uiimage = nil;
+if (imageOrientation)
+{
+    //create a UIImage
+    
+    uiimage =
+    [UIImage imageWithCGImage:cgiimg
+                        scale:1.0f
+                  orientation:(UIImageOrientation)imageOrientation];
+    
+}
+else
+{
+    //create a UIImage
+    uiimage =
+    [UIImage imageWithCGImage:cgiimg
+                        scale:1.0f
+                  orientation:(UIImageOrientation)UIImageOrientationUp];
+    
+}
+
+
+[(UIImageView*)[[[self view]subviews] objectAtIndex:0] setImage:uiimage];
 }
 
 -(void) applyAnImageToTheViewsLayer
@@ -140,10 +304,159 @@
 
 }
 
+void imageMetadataFetcher (CIImage* image)
+{
+    //get
+    NSDictionary* tiffDictionary = nil;
+    tiffDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyTIFFDictionary];
+    
+    NSDictionary* gifdDictionary = nil;
+    gifdDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyGIFDictionary];
+    
+    NSDictionary* jfifDictionary = nil;
+    jfifDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyJFIFDictionary];
+    
+    
+    NSDictionary* ExifDictionary = nil;
+    ExifDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyExifDictionary];
+    
+    NSDictionary* PNGDictionary = nil;
+    PNGDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyPNGDictionary];
+    
+    
+    NSDictionary* IPTCDictionary = nil;
+    IPTCDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyIPTCDictionary];
+    
+    NSDictionary* GPSDictionary = nil;
+    GPSDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyGPSDictionary];
+    
+    NSDictionary* RawDictionary = nil;
+    RawDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyRawDictionary];
+    
+    NSDictionary* CIFFDictionary = nil;
+    CIFFDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyCIFFDictionary];
+    
+    
+    /*
+     Camera Makers
+     */
+    NSDictionary* canonDictionary = nil;
+    canonDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyMakerCanonDictionary];
+    
+//    NSDictionary* nikonDictionary = nil;
+//    nikonDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyMakerNikonDictionary];
+//    
+//    NSDictionary* minoltaDictionary = nil;
+//    minoltaDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyMakerMinoltaDictionary];
+//    
+//    NSDictionary* fujiDictionary = nil;
+//    fujiDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyMakerFujiDictionary];
+//    
+//    NSDictionary* olympusDictionary = nil;
+//    olympusDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyMakerOlympusDictionary];
+//    
+//    NSDictionary* pentaxDictionary = nil;
+//    pentaxDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyMakerPentaxDictionary];
+    
+    //others
+    
+    NSDictionary* BIMDictionary = nil;
+    BIMDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImageProperty8BIMDictionary];
+    
+    NSDictionary* DNGDictionary = nil;
+    DNGDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyDNGDictionary];
+    
+    NSDictionary* ExifAuxDictionary = nil;
+    ExifAuxDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyExifAuxDictionary];
+    
+    //not on iphone
+//    NSDictionary* OpenEXRDictionary = nil;
+//    pOpenEXRDictionary =
+//    [[image properties]
+//     objectForKey:(__bridge NSString*) kCGImagePropertyOpenEXRDictionary];
+    
+    //ios7 and above
+    NSDictionary* MakerAppleDictionary = nil;
+    MakerAppleDictionary =
+    [[image properties]
+     objectForKey:(__bridge NSString*) kCGImagePropertyMakerAppleDictionary];
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+void getallDataFromCGImage (CGImageRef image)
+{
+    
+    __unused CGImageAlphaInfo alphaInfo =
+    CGImageGetAlphaInfo(image);
+    
+    CGImageGetBitmapInfo(image);
+    
+    CGImageGetBitsPerComponent(image);
+    
+    CGImageGetBitsPerPixel(image);
+    
+    CGImageGetBytesPerRow(image);
+    
+    CGImageGetColorSpace(image);
+    
+    CGImageGetDataProvider(image);
+    
+    CGImageGetDecode(image);
+    
+    CGImageGetHeight(image);
+    
+    CGImageGetRenderingIntent(image);
+    
+    CGImageGetShouldInterpolate(image);
+    
+    CGImageGetTypeID();
+    
+    CGImageGetWidth(image);
+    
+    CGImageIsMask(image);
+    
 }
 
 @end
